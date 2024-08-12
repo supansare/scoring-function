238f159ff053a54db0f52a98e11880bcc799e0f3
supansare <85484370+supansare@users.noreply.github.com>
2024-08-12 19:29:37 +0530

    Update main.py

--- churn-prediction/main.py
+++ churn-prediction/main.py
@@ -1,7 +1,17 @@
-import os
-from ibm_watson_machine_learning import APIClient
+import json
+#import json, requests
 
 def main(params):
+  header = {"Content-Type": "application/json", "x":"y"} 
+  scoring_url="http://169.60.148.132:9443/predictions"
+  data = [['Male','No','No','No',2,'Yes','No','DSL','Yes','Yes','No','No','No','No','Month-to-month','Mailed check',53.85,3239]]
+  params = {
+    "input_data": [{
+        "values" : data
+    }]
+  }
+#  scoring_response = requests.post(scoring_url, json=params, headers=header, verify=False)
+#   jsonify_scoring_response = scoring_response.json()
 
   return {
         "headers": {
