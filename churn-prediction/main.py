2d452a64751b7d2011586ba1a15eb33b11c404b6
supansare <85484370+supansare@users.noreply.github.com>
2024-08-12 18:58:18 +0530

    Create main.py

---
+++ churn-prediction/main.py
@@ -0,0 +1,11 @@
+import os
+
+def main(params):
+
+  return {
+        "headers": {
+            "Content-Type": "application/json",
+        },
+        "statusCode": 200,
+        "body": "hello",
+  }
